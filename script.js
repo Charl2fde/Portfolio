5e813c74ad0c8ffc6c0fd44a43a4e198e14d5670
Chido Le Riz <charlesdefde@gmail.com>
2023-02-27 15:57:11 +0100

    télécharger nul

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ window.addEventListener('scroll', function() {
     const scrolled = (scrollPosition / (fullHeight - windowHeight)) * 100;
     document.getElementById('progressBar2').style.width = scrolled + '%';
   });
+
+const downloadLinks = document.querySelectorAll("[data-download]");
+
+  downloadLinks.forEach(button =>{
+      const id = button.dataset.download;
+      const image = document.getElementById(id);
+      const a = document.createElement("a");
+
+      a.href = imgage.src;
+      a.download = "";
+      a.style.display = "none";
+  })
