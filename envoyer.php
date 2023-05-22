acd9a7e3140b50c18f755d552e472f7f230d24e1
Chido Le Riz <99027192+Charl2fde@users.noreply.github.com>
2023-05-22 20:49:21 +0200

    formulaire

--- envoyer.php
+++ envoyer.php
@@ -3,13 +3,13 @@
 $host = "localhost";
 $dbname = "id20606176_form_portfolio";
 $username = "id20606176_charles";
-$password = "Chatelard0300$"; 
+$password = "Chatelard0300$";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // Définit le mode d'erreur PDO à Exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
     die();
 }
@@ -32,25 +32,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bindParam(':message', $message);
     $stmt->execute();
 
-    if (mysqli_query($connexion, $sql)) {
-        // Envoi des données par e-mail
-        $destinataire = 'charlesdefde@gmail.com';
-        $sujet = 'Nouveau formulaire soumis';
-        $contenu = "Nom: $nom\n";
-        $contenu .= "Prénom: $prenom\n";
-        $contenu .= "E-mail: $email\n";
-        $contenu .= "Téléphone: $telephone\n";
-        $contenu .= "Message: $message\n";
-    
-        // Envoyer l'e-mail
-        mail($destinataire, $sujet, $contenu);
-    
-        // Redirection vers une page de confirmation
-        header("Location: index.html");
-    } else {
-        echo "Erreur lors de l'insertion des données : " . mysqli_error($connexion);
-    }
-    
-    // Fermer la connexion à la base de données
-    mysqli_close($connexion);
+    // Envoi des données par e-mail
+    $destinataire = 'charlesdefde@gmail.com';
+    $sujet = 'Nouveau formulaire soumis';
+    $contenu = "Nom: $nom\n";
+    $contenu .= "Prénom: $prenom\n";
+    $contenu .= "E-mail: $email\n";
+    $contenu .= "Téléphone: $telephone\n";
+    $contenu .= "Message: $message\n";
+
+    // Envoyer l'e-mail
+    mail($destinataire, $sujet, $contenu);
+
+    // Redirection vers une page de confirmation
+    header("Location: index.html");
+}
 ?>
+
